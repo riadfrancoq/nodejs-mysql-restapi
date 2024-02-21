27135d26c44aab51492078f43916818c53be7a46
campusricardo <campusricardo2023@gmail.com>
2024-02-20 23:47:36 -0500

    fix: :sparkles:

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -4,6 +4,7 @@ import { pool } from '../db.js';
 export const getEmployees = async(req, res)=> {
     try {
     const [rows] = await pool.query('SELECT * FROM employee');
+    console.log(rows);
     res.json(rows);
     } catch (error) {
         console.log(error)
