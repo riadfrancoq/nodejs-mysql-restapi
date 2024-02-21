053f57d01388114ba96c87a983dea65fdfd797f2
campusricardo <campusricardo2023@gmail.com>
2024-02-20 23:32:42 -0500

    fix: :bug: Add try catch to ping

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -2,6 +2,13 @@
 import { pool } from '../db.js';
 
 export const ping = async (req, res ) => { 
-    const [result] = await pool.query('SELECT 1 + 1 AS result')
-    res.json(result[0]);
+    try {
+        const [result] = await pool.query('SELECT 1 + 1 AS result');
+        res.json(result[0]);
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            message: "Something goes wrong"
+        });
+    }
 }
